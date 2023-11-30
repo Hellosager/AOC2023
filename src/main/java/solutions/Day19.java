6b032956c0c8f4c4f5cc02ef14e85b12297df6a4
Hellosager <sebas.b@web.de>
2023-11-30 21:53:02 +0100

    init

---
+++ src/main/java/solutions/Day19.java
@@ -0,0 +1,18 @@
+package solutions;
+
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+public class Day19 {
+    public static void main(String[] args) throws FileNotFoundException {
+        readInputFile().forEach(System.out::println);
+    }
+
+    private static List<String> readInputFile() throws FileNotFoundException {
+        return IOUtils.readLines(new FileReader("src/main/resources/inputs/input01.txt"));
+    }
+}
